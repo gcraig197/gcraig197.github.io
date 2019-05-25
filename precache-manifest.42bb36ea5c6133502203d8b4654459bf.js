3fd1ac90efde1e9133e67b8c26e543b177629496
Gavin Craig <gavin.craig.2015@uni.strath.ac.uk>
2019-05-25 16:49:21 +0100

    Updates

---
+++ precache-manifest.42bb36ea5c6133502203d8b4654459bf.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317a0bb13bf6f12e4e6f57b4819afc17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c2e29c31bfdf65eae1",
+    "url": "/static/css/2.94155858.chunk.css"
+  },
+  {
+    "revision": "ab7f0760686ae18c4559",
+    "url": "/static/css/main.9b44463e.chunk.css"
+  },
+  {
+    "revision": "f2c2e29c31bfdf65eae1",
+    "url": "/static/js/2.8092b997.chunk.js"
+  },
+  {
+    "revision": "ab7f0760686ae18c4559",
+    "url": "/static/js/main.bab86498.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "549c9ad4b95b57e5b2b5cfb254909ddc",
+    "url": "/static/media/Giant_squirrel_pet.549c9ad4.png"
+  },
+  {
+    "revision": "586cb87c86bd8fe4cde34caabfeab7bf",
+    "url": "/static/media/apartment-chair-contemporary-509922.586cb87c.jpg"
+  },
+  {
+    "revision": "2466b16031a401a6187eafb507c22c66",
+    "url": "/static/media/architecture-astronomy-bird-s-eye-view-1820563.2466b160.jpg"
+  },
+  {
+    "revision": "a853a9b9d691db3524fe54bc5dae76e8",
+    "url": "/static/media/buildings-clear-sky-exterior-374023.a853a9b9.jpg"
+  },
+  {
+    "revision": "b4d3fd830270bc3ccb0b8e1fbe4ed52e",
+    "url": "/static/media/cup-desk-drink-434337.b4d3fd83.jpg"
+  },
+  {
+    "revision": "c1598b33560a4cf1f066bd3c283bc6fb",
+    "url": "/static/media/device-electronics-keyboard-872958.c1598b33.jpg"
+  },
+  {
+    "revision": "2f669cfda677d661b009c8b1d6a405fa",
+    "url": "/static/media/honours-app-3-displays-01.2f669cfd.png"
+  },
+  {
+    "revision": "c267d1b381c815d8f00171435793fa86",
+    "url": "/static/media/honours-project-displays.c267d1b3.jpg"
+  },
+  {
+    "revision": "ef555e181dcc0faca8a3659b6640abc1",
+    "url": "/static/media/iphone-app-mockups-01.ef555e18.png"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "a6330e576b750a4f69af4f757ca8f230",
+    "url": "/static/media/swshr-jumpers.a6330e57.jpg"
+  },
+  {
+    "revision": "1f123d23e8fabe55af03aa54fe26247d",
+    "url": "/static/media/swshr-tee-shirts.1f123d23.JPG"
+  }
+]);
